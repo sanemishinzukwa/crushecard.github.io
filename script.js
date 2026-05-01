8702d8cb8924acec7d37cbb9106237c11fa7386c
sanemishinzukwa <sanemishinzukwa@gmail.com>
2026-05-01 11:30:05 +0530

    basic website is done now will add animation

---
+++ script.js
@@ -0,0 +1,92 @@
+const mymsg = [{
+title: "Hey My Dear Crush, This is the First Message of your e-card",
+image: "./assets/chick.png",
+
+
+},
+{
+    title: "I Always Admire",
+    text: "Want to Let you Know that I Always Admire you From a Distance",
+    image: "./assets/butterflies.png"
+},
+{
+    title: "When First Time I Saw YOu",
+    text: "I Stucked and Freeze Looking at You",
+    image: "./assets/photo-camera.png",
+},
+{
+    title: "Try To Confess",
+    text: "When Ever I Tried to Confess I Hesitate Every time.",
+    image: "./",
+
+},
+{
+    title: "I Got Courage!",
+    text: "Finally I Got the Courage To Tell You.",
+    image: "./assets/cat.png"
+},
+{
+    title: "Confession To You",
+    text: "I Like you So Much from the Day I saw you once helping the children and Playing Like a child",
+    image: ""
+},
+{
+    title: "The Day I Fall For you",
+    text: "From That Day, I Admire you Secretly and Thinking About you Only.",
+    image: ""
+},
+{
+    title: "So I created this E-card",
+    text: "To Convey my Felling to You and How you like about it.Please Let me Know",
+    image: "./assets/cloud.png",
+
+},
+{
+    title: "Thanks For Listening My Feeling",
+    text: "I will be Waiting For your Response",
+    image: ""
+},
+{
+    title: "Byee Love You So Much",
+    text: "Finally I feeling Light like a Cloud after sharing my words with you.",
+    image: ""
+}
+]
+
+let currentIndex = 0;
+
+const prebtn = document.getElementById("prebtn");
+const nextbtn = document.getElementById("nextbtn");
+
+const title = document.getElementById("title");
+const cardtext = document.getElementById("cardtext");
+
+
+const imgs = document.getElementById("imgs");
+
+function Textmsg(index) {
+    title.textContent = mymsg[index].title;
+        cardtext.textContent = mymsg[index].text;
+
+    imgs.src = mymsg[index].imgs;
+
+    prebtn.disabled = index === 0;
+    nextbtn.disabled = index === mymsg.length - 1;
+}
+
+
+
+
+nextbtn.addEventListener ("click", ()=>{
+    if (currentIndex < mymsg.length - 1){
+        currentIndex++ ;
+        Textmsg(currentIndex);
+    }
+});
+prebtn.addEventListener ("click", ()=>{
+    if (currentIndex > 0){
+        currentIndex--;
+        Textmsg(currentIndex);
+    }
+});
+Textmsg(currentIndex);
